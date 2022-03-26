14200733dad687a5c577fc5641e2cbf1f49d678c
Allen C <allenc@Zipcoders-MacBook-Pro.local>
2022-03-25 21:15:08 -0400

    Finished Part B, read_all_json_files.

--- json_helper.py
+++ json_helper.py
@@ -2,7 +2,8 @@ import os
 import json
 
 
-def read_json():
+# Part A
+def read_json(file_path):
     file_path = os.path.abspath('mario.json')
 
     # opening JSON file
@@ -11,3 +12,24 @@ def read_json():
     # returns JSON object as dict
     data = json.load(f)
     return data
+
+
+# Part B
+def read_all_json_files():
+    file_path = os.path.abspath('mario.json')
+    file_path1 = os.path.abspath('link.json')
+    file_path2 = os.path.abspath('superman.json')
+    file_path3 = os.path.abspath('thor.json')
+    file_path4 = os.path.abspath('goku.json')
+    file_path5 = os.path.abspath('krillin.json')
+
+    obj1 = read_json(file_path)
+    obj2 = read_json(file_path1)
+    obj3 = read_json(file_path2)
+    obj4 = read_json(file_path3)
+    obj5 = read_json(file_path4)
+    obj6 = read_json(file_path5)
+
+    list_of_files = [obj1, obj2, obj3, obj4, obj5, obj6]
+    return list_of_files
+
